0235ee27106b55f275402f4a86f28371d48a65ae
linnahan <1596695476@qq.com>
2020-12-09 22:14:49 +0800

    20/12/9

--- 学习/字符串的分割.py
+++ 学习/字符串的分割.py
@@ -1,8 +1,10 @@
-a="I'm a student!"
-b=a.split()              #分割a后的序列是一个新的定义，不再是a
-print(a)
+a = "I'm a student!"
+b = a.split()              #分割a后的序列是一个新的定义，不再是a
 print(b)
-for i in b:
-    print(i)
-c='hi.I\'m a student.hello'
+c = 'hi.I\'m a student.hello'
 print(c.split('.'))
+
+#或者用正则
+import re
+d = re.split(r' ',a)
+print(d)
