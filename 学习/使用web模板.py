188872112580a7b9d2beeabf98b4908cc4341b94
linnahan <1596695476@qq.com>
2021-01-10 23:53:53 +0800

    21/1/10

---
+++ 学习/使用web模板.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return render_template('home.html')
+
+@app.route('/signin', methods=['GET'])
+def signin_form():
+    return render_template('form.html')
+
+@app.route('/signin', methods=['POST'])
+def signin():
+    username = request.form['username']
+    password = request.form['password']
+    if username=='admin' and password=='password':
+        return render_template('signin-ok.html', username=username)
+    return render_template('form.html', message='Bad username or password', username=username)
+
+if __name__ == '__main__':
+    app.run()
+    
+'''这样，我们就成功地把Python代码和HTML代码最大限度地分离了。
+使用模板的另一大好处是，模板改起来很方便，而且，改完保存后，刷新浏览器就能看到最新的效果
+
+除了Jinja2，常见的模板还有：
+Mako：用<% ... %>和${xxx}的一个模板；
+Cheetah：也是用<% ... %>和${xxx}的一个模板；
+Django：Django是一站式框架，内置一个用{% ... %}和{{ xxx }}的模板。
+'''
